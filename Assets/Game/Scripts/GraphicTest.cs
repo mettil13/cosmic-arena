0c190ddeb5995ae97eb07931712df7f7d8773f25
ElanProject <126772054+ElanSSHProject@users.noreply.github.com>
2025-01-29 13:45:31 +0100

    player physics response

--- Assets/Game/Scripts/GraphicTest.cs
+++ Assets/Game/Scripts/GraphicTest.cs
@@ -1,12 +1,65 @@
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class GraphicTest : MonoBehaviour
+namespace physics
 {
-    [SerializeField] private Rigidbody body;
-
-    private void Awake()
+    public class GraphicTest : MonoBehaviour
     {
-        body = GetComponent<Rigidbody>();
-        body.AddTorque(Random.insideUnitSphere * 100);
+        [SerializeField] private Rigidbody body;
+        [SerializeField] private bool controlled = true;
+
+        public bool Controlled
+        {
+            set
+            {
+                if (value == controlled) return;
+
+                if (value)
+                {
+                    body.constraints = RigidbodyConstraints.FreezeAll;
+                }
+                else
+                {
+                    body.constraints = RigidbodyConstraints.FreezePosition;
+                    transform.DOKill();
+                }
+
+                controlled = value;
+            }
+            get => controlled;
+        }
+
+        public void Init()
+        {
+            body = GetComponent<Rigidbody>();
+            Controlled = true;
+        }
+        public void ApplyRandomTorque(float intensity)
+        {
+            body.angularVelocity = Vector3.zero;
+            //Debug.LogError("angular velocity : " + body.angularVelocity);
+            body.AddTorque(Random.insideUnitSphere * intensity);
+        }
+        public void InvertTorque()
+        {
+            body.angularVelocity = -body.angularVelocity;
+        }
+        public void ResetTorque()
+        {
+            body.angularVelocity = Vector3.zero;
+        }
+        public void ApplyDirection(Vector2 direction)
+        {
+            Vector3 rotation = new Vector3(0, -Vector2.SignedAngle(Vector2.right, direction), 0);
+            transform.DOKill();
+            transform.DORotate(rotation, 2);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            //Debug.Log("a");
+        }
     }
 }
--- Assets/Game/Scripts/inputTest.cs
+++ Assets/Game/Scripts/inputTest.cs
@@ -1,31 +1,97 @@
+using System.Collections;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class inputTest : MonoBehaviour
+namespace physics
 {
-    public Rigidbody myRigidbody;
-    public float speedMultipier = 1;
-    public Transform cursor;
-    float lastTimeClicked = 0;
-    public float coolDown = 0.5f;
-    private Vector2 direction;
+    [System.Serializable] public struct ChildTorqueInfo
+    {
+        [SerializeField] public GraphicTest graphicBody;
+        [SerializeField] public float startTorqueIntensity;
+        [SerializeField] public float collisionTorqueIntensity;
+        [SerializeField] public float MoveTorqueIntensity;
+        [SerializeField] public float nonControlTime;
+    }
+    [System.Serializable] public struct MovementInfo
+    {
+        [SerializeField] public Rigidbody body;
+        [SerializeField] public float movementIntensity;
+    }
 
-    private void Update() {
-        float horizontal;
-        float vertical;
+    public class inputTest : MonoBehaviour
+    {
+        public Transform cursor;
+        float lastTimeClicked = 0;
+        public float coolDown = 0.5f;
+        private Vector2 direction;
 
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        [SerializeField] MovementInfo movementInfo;
+        [SerializeField] ChildTorqueInfo childTorqueInfo;
 
-        if (horizontal != 0 || vertical != 0) {
-            direction = new Vector2(horizontal, vertical).normalized;
+        private Coroutine nonControlCR;
+
+        private void Awake()
+        {
+            childTorqueInfo.graphicBody.Init();
+            childTorqueInfo.graphicBody.ApplyRandomTorque(childTorqueInfo.startTorqueIntensity);
+        }
+        private void Update()
+        {
+            Vector2 currentDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+            if (currentDirection.x != 0 || currentDirection.y != 0)
+            {
+                if (childTorqueInfo.graphicBody.Controlled && currentDirection != direction)
+                {
+                    childTorqueInfo.graphicBody.ApplyDirection(currentDirection);
+                }
+
+                direction = currentDirection;
+            }
+
+            cursor.eulerAngles = new Vector3(0, -Vector2.SignedAngle(Vector2.right, direction), 0);
+            childTorqueInfo.graphicBody.transform.localPosition = Vector3.zero;
+
+            if (Input.GetKeyDown(KeyCode.Return) && Time.time - lastTimeClicked > coolDown)
+            {
+                ApplyNonControl();
+                MoveWithVector(direction);
+                lastTimeClicked = Time.time;
+            }
         }
 
-        cursor.eulerAngles = new Vector3(0, -Vector2.SignedAngle(Vector2.right, direction), 0);
+        public void MoveWithVector(Vector2 movementDirection)
+        {
+            movementInfo.body.AddForce(
+                new Vector3(movementDirection.x, 0, movementDirection.y) * movementInfo.movementIntensity, 
+                ForceMode.Impulse);
+            childTorqueInfo.graphicBody.ApplyRandomTorque(childTorqueInfo.MoveTorqueIntensity);
+        }
+        public void MoveWithVectorReset(Vector2 movementDirection)
+        {
+            movementInfo.body.linearVelocity = Vector3.zero;
+            MoveWithVector(movementDirection);
+        }
 
-        if (Input.GetKeyDown(KeyCode.Return) && Time.time - lastTimeClicked > coolDown) {
-            myRigidbody.AddForce(new Vector3(direction.x, 0, direction.y) * speedMultipier, ForceMode.Impulse); //velocity o addforce?
-            lastTimeClicked = Time.time;
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.transform.tag == "Wall")
+            {
+                movementInfo.body.angularVelocity = -movementInfo.body.angularVelocity;
+                childTorqueInfo.graphicBody.ApplyRandomTorque(childTorqueInfo.collisionTorqueIntensity);
+            }
+        }
+
+        public void ApplyNonControl()
+        {
+            if (nonControlCR != null) StopCoroutine(nonControlCR);
+            nonControlCR = StartCoroutine(NonControlCR());
+        }
+        public IEnumerator NonControlCR()
+        {
+            childTorqueInfo.graphicBody.Controlled = false;
+            yield return new WaitForSeconds(childTorqueInfo.nonControlTime);
+            childTorqueInfo.graphicBody.Controlled = true;
         }
     }
 }
