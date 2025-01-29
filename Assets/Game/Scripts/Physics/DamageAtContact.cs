a38bf68cb3d8d6b9605261234ebc3838939c9154
AmyPon14 <amanda.ponziani.2000@gmail.com>
2025-01-29 16:12:38 +0100

    primi props

---
+++ Assets/Game/Scripts/Physics/DamageAtContact.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DamageAtContact : MonoBehaviour
+{
+    public bool disappearAfterContact;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Debug.Log("danno al player");
+        }
+        if (disappearAfterContact) Disappear();
+    }
+
+    private void Disappear() {
+        gameObject.SetActive(false);
+    }
+}
