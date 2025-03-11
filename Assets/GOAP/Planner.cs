d76b5ac8657fa9a1a6cf7bd4319ad49dba1bd919
DavideArruzzoli <davide@arruzzoli.it>
2025-03-11 21:27:54 +0100

    AI wip

--- Assets/GOAP/Planner.cs
+++ Assets/GOAP/Planner.cs
@@ -2,6 +2,7 @@ using Sirenix.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 namespace GOAP
@@ -27,15 +28,15 @@ namespace GOAP
         }
 
 
-        public GOAPAction[] Plan(BlackBoard beliefs, BlackBoard goal)
+        public List<GOAPAction> Plan(BlackBoard beliefs, BlackBoard goal)
         {
-
+            List<GOAPAction> plan = AStar(beliefs, goal, Node.Heuristic);
 
             return plan;
         }
 
         public delegate float Heuristic<T> (T a, T b);
-        public GOAPAction[] AStar(BlackBoard startState, BlackBoard goal, Heuristic<BlackBoard> heuristic)
+        public List<GOAPAction> AStar(BlackBoard startState, BlackBoard goal, Heuristic<BlackBoard> heuristic)
         {
             List<Node> openList = new List<Node>() { new Node(startState, null, 0, heuristic(startState, goal), null) };
             List<Node> closedList = new List<Node>();
@@ -44,34 +45,45 @@ namespace GOAP
             {
                 openList.Sort((Node a, Node b) => { return (a.f < b.f) ? -1 : a.f > b.f ? 1 : 0; });
                 Node node = openList[0];
+                if (node.h == 0)
+                {
+                    return ReconstructPath(node);
+                }
                 closedList.Add(node);
 
-                foreach(GOAPAction action in actions)
+                foreach (GOAPAction action in actions)
                 {
                     BlackBoard newState = action.CalculateEffect(node.state);
+
                     Node newNode = new Node(newState, action, node.g + action.weight, heuristic(newState, goal), node);
                     //check better path
                     openList.Add(newNode);
+
+
                     
                 }
 
             }
 
 
-            return new GOAPAction[0];
+            return null;
 
             
         }
-        GOAPAction[] ReconstructPath(List<Node> closedList)
+        List<GOAPAction> ReconstructPath(Node current)
         {
-            List<GOAPAction> path = new List<GOAPAction>();
-            foreach (Node node in closedList)
-            {
-                path.Add(node.doneAction);
-            }
-            return path.ToArray();
-        }
+            List<GOAPAction> path = new List<GOAPAction>() {current.doneAction};
 
+            while (current.parent != null)
+            {
+                current = current.parent;
+                path.Add(current.doneAction);
+            }
+
+            path.Reverse();
+            return path;
+
+        }
 
         public class Node
         {
@@ -80,7 +92,7 @@ namespace GOAP
             public float g;
             public float h;
             public float f => g + h;
-            Node parent;
+            public Node parent;
 
             public Node(BlackBoard state, GOAPAction doneAction, float g, float h, Node parent)
             {
@@ -89,6 +101,25 @@ namespace GOAP
                 this.g = g;
                 this.h = h;
                 this.parent = parent;
+            }
+
+            public static float Heuristic(BlackBoard from, BlackBoard to) 
+            {
+                float h = 0;
+                foreach(WorldState objective in to.worldStates.Values) 
+                {
+                    if (from.worldStates.TryGetValue(objective.name, out WorldState state) && objective == state)
+                    {
+
+                    }
+                    else
+                    {
+                        h += 1;
+                    }
+                }
+
+
+                return h;
             }
         }
     }
--- Assets/GOAP/WorldState.cs
+++ Assets/GOAP/WorldState.cs
@@ -8,11 +8,29 @@ namespace GOAP
         public string name;
         public bool isActive;
         public float value;
+        public float valueThreshold;
         public Vector3 position;
+        public float positionSqrThreshold;
 
         internal void ApplyEffect(WorldState effect)
         {
             throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Uses the thresholds of a
+        /// </summary>
+        public static bool operator == (WorldState a, WorldState b)
+        {
+            return  (a.isActive && b.isActive) && 
+                    ((a.position - b.position).sqrMagnitude < a.positionSqrThreshold) && 
+                    (MathF.Abs(a.value - b.value) < a.valueThreshold);
+        }
+        /// <summary>
+        /// Uses the thresholds of a
+        /// </summary>
+        public static bool operator != (WorldState a, WorldState b)
+        {
+            return !(a == b);
         }
     }
 
