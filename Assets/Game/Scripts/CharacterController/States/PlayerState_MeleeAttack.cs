9902ae92160b752294e94fdf0a8e26bd40b31ae2
Davide Arruzzoli <davide@arruzzoli.it>
2025-04-08 16:16:57 +0200

    attack fix e mindbender

--- Assets/Game/Scripts/CharacterController/States/PlayerState_MeleeAttack.cs
+++ Assets/Game/Scripts/CharacterController/States/PlayerState_MeleeAttack.cs
@@ -15,6 +15,8 @@ namespace CharacterLogic
         [SerializeField] float damage = 1;
         private Vector2 initialDirection = Vector2.zero;
 
+        private bool attacked = false;
+
         Timer timer;
 
         public override void Init(CharacterManager characterManager)
@@ -29,33 +31,39 @@ namespace CharacterLogic
             timer = new Timer(attackDuration).AddCallBack(Stop);
             characterManager.animator.CrossFade("Attack", characterManager.fadeTime);
             initialDirection = new Vector2(Input.LastValidDirection.x, Input.LastValidDirection.y);
+            attacked = false;
         }
 
         public override void OnUpdate(ref float delta) {
             base.OnUpdate(ref delta);
             timer.Update(ref delta);
+            if(!attacked)
+            {            
+                Vector3 inputDirection = new Vector3(initialDirection.x, 0, initialDirection.y);
+                Vector3 capsuleCheckHead = characterManager.transform.position + Quaternion.FromToRotation(Vector3.right, inputDirection) * new Vector3(attackOffsetX, 0, attackOffsetY + attackAreaY / 2); 
+                Vector3 capsuleCheckTail = characterManager.transform.position + Quaternion.FromToRotation(Vector3.right, inputDirection) * new Vector3(attackOffsetX, 0, attackOffsetY - attackAreaY / 2); 
+                Debug.DrawLine(capsuleCheckHead, characterManager.transform.position, Color.yellow, 1);
+                Debug.DrawLine(capsuleCheckTail, characterManager.transform.position, Color.yellow, 1);
+                Debug.DrawLine(capsuleCheckHead, capsuleCheckTail, Color.yellow, 1);
+                //Debug.Log(inputDirection);
+                //Debug.Log(capsuleCheckHead + " " +  capsuleCheckTail + " " + characterManager.transform.position);
+                Collider[] overlappingColliders = Physics.OverlapCapsule(capsuleCheckHead, capsuleCheckTail, attackAreaX / 2, LayerMask.GetMask("Player"));
+                    if (overlappingColliders.Length > 0)
+                    {
+                        //Debug.Log(overlappingColliders[0].name);
+                        CharacterManager character = overlappingColliders[0].GetComponent<CharacterManager>();
+                        CharacterHealth characterHealth = overlappingColliders[0].GetComponent<CharacterHealth>();
+                        if (character != null && character != characterManager)
+                        {
+                            if (characterHealth != null)
+                            {
+                                characterHealth.TakeDamage(damage, characterManager.gameObject);
+                            }
 
-            Vector3 inputDirection = new Vector3(initialDirection.x, 0, initialDirection.y);
-            Vector3 capsuleCheckHead = characterManager.transform.position + Quaternion.FromToRotation(Vector3.right, inputDirection) * new Vector3(attackOffsetX, 0, attackOffsetY + attackAreaY / 2); 
-            Vector3 capsuleCheckTail = characterManager.transform.position + Quaternion.FromToRotation(Vector3.right, inputDirection) * new Vector3(attackOffsetX, 0, attackOffsetY - attackAreaY / 2); 
-            Debug.DrawLine(capsuleCheckHead, characterManager.transform.position, Color.yellow, 1);
-            Debug.DrawLine(capsuleCheckTail, characterManager.transform.position, Color.yellow, 1);
-            Debug.DrawLine(capsuleCheckHead, capsuleCheckTail, Color.yellow, 1);
-            //Debug.Log(inputDirection);
-            //Debug.Log(capsuleCheckHead + " " +  capsuleCheckTail + " " + characterManager.transform.position);
-            Collider[] overlappingColliders = Physics.OverlapCapsule(capsuleCheckHead, capsuleCheckTail, attackAreaX / 2, LayerMask.GetMask("Player"));
-            if(overlappingColliders.Length > 0) {
-                //Debug.Log(overlappingColliders[0].name);
-                CharacterManager character = overlappingColliders[0].GetComponent<CharacterManager>();
-                CharacterHealth characterHealth = overlappingColliders[0].GetComponent<CharacterHealth>();
-                if(character != null && character != characterManager) {
-                    if(characterHealth != null) {
-                        characterHealth.TakeDamage(damage, characterManager.gameObject);
+                            character.stateMachine.ChangeState(Player_State.Stunned);
+                        }
+                        attacked = true;
                     }
-
-                    character.stateMachine.ChangeState(Player_State.Stunned);
-                }
-                Stop();
             }
 
         }
